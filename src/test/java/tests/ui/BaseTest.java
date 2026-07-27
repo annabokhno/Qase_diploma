1e5233e5c7087b65503ad71ace81416aaf8b99dd
Borodich Timofei <borodich.timofei@gmail.com>
2026-07-27 11:10:10 +0500

    remove property

--- src/test/java/tests/ui/BaseTest.java
+++ src/test/java/tests/ui/BaseTest.java
@@ -21,11 +21,11 @@ public class BaseTest {
 
     protected static final String EMAIL =
             System.getenv("QASE_EMAIL") != null
-                    ? System.getenv("QASE_EMAIL")
+                    ? System.getProperty("QASE_EMAIL")
                     : PropertyReader.getProperty("QASE_EMAIL");
     protected static final String PASSWORD =
             System.getenv("QASE_PASSWORD") != null
-                    ? System.getenv("QASE_PASSWORD")
+                    ? System.getProperty("QASE_PASSWORD")
                     : PropertyReader.getProperty("QASE_PASSWORD");
 
     LoginPage loginPage;
