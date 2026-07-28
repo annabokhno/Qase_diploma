a97dd61a575449659c1611f0e77afa20fb36ced1
annabokhnowork-sudo <anna.bokhno.work@gmail.com>
2026-07-28 22:32:51 +0300

    Fix_secrets_7

--- src/main/java/ui/pages/ProjectsPage.java
+++ src/main/java/ui/pages/ProjectsPage.java
@@ -22,7 +22,7 @@ public class ProjectsPage {
     @Step("Нажать кнопку создания нового проекта")
     public CreateProjectPage clickCreateProject() {
         log.info("Clicking Create new project button");
-        $(byText("Create new project")).shouldBe(Condition.visible).click();
+        $(byText("Create new project")).click();
         return new CreateProjectPage();
     }
 
